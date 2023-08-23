459484d56a1103c69fe32e074d5662349c4c4edc
gozonjoedaimar <gozonjoedaimar@gmail.com>
2023-08-23 16:44:25 +0800

    Setup for windows

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+# -- UNCOMMENT and UPDATE path FOR WINDOWS
+# sys.path.insert(0, os.path.abspath('%PYTHON_INSTALL_PATH_HERE%/LocalCache/local-packages/Python311/site-packages'))
 
 
 # -- Project information -----------------------------------------------------
